f7f7f8fb226f538b7aaa0c1c93d331aa6d8690d8
El Habib <Abdelaziz.59140@gmail.com>
2020-04-12 17:12:07 +0200

    add exercices 1

---
+++ DOM (1)/exoDebutantMoyenHard/Correction-ReadMe/README-1.js
@@ -0,0 +1,25 @@
+//exercise1
+var number = 57;
+var number1 = 23;
+var number2 = 98;
+var number3 = 67;
+
+function biggestNumber(number, number1, number2, number3) {
+    if (number > number1 && number > number2 && number > number3) 
+    {
+      console.log(a);
+    } 
+    else if (number1 > number && number1 > number2 && number1 > number3) 
+    {
+      console.log(b)
+    } 
+    else if (number2 > number1 && number2 > number1 && number2 > number3) 
+    {
+      console.log(c);
+    } 
+    else 
+    {
+      console.log(d);
+    }
+  }
+  biggestNumber(number, number1, number2, number3);
