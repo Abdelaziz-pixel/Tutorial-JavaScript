445bb0199898617e0d10d7f0942d6803271c8431
El Habib <Abdelaziz.59140@gmail.com>
2020-04-12 17:24:10 +0200

    add exercices 9

--- DOM (1)/exoDebutantMoyenHard/Correction-ReadMe/README-1.js
+++ DOM (1)/exoDebutantMoyenHard/Correction-ReadMe/README-1.js
@@ -104,3 +104,23 @@ while (n != 6)
     console.log(n*symbol)
     n ++;
 }
+
+//exercise9
+for (var i = 1; i <= 100; i++) 
+{
+    if (i % 15 === 0) 
+    {
+      console.log("FIZZBUZZ");
+    } 
+    else if (i % 5 === 0) 
+    {
+      console.log("BUZZ");
+    } 
+    else if (i % 3 === 0) {
+      console.log("FIZZ");
+    } 
+    else 
+    {
+      console.log(i);
+    }
+}
